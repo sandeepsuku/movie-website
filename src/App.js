9dfbe9ec8d44330b2dd57e63c4f5fa0a62b378e7
“Sandeep <sandeepsuku@gmail.com>
2022-07-20 16:37:08 -0400

    Created Components with basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.scss';
+import { BrowserRouter as Router,} from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Movie Website !! </h1>
+       <Router>
+         <Header></Header>
+         <Home></Home>
+         <Footer></Footer>
+       </Router>
     </div>
   );
 }
