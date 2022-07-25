33c401c0b14d1dc2689f5add38fe9c3fcbe7dbd5
chamberlince@icloud.com <chamberlince@icloud.com>
2022-07-25 14:17:21 -0400

    added import

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import {getMovieById, getAllMovies} from '../utils/movieList.js'
+import MovieListing from '../movieListing/MovieListing'
 
 const allMovies = getAllMovies();
 console.log(allMovies);
 const movieData = getMovieById('tt0314166');
 console.log(movieData);
 
-import MovieListing from '../movieListing/MovieListing'
+
 export class Home extends Component {
   render() {
     return (
       <div style={{ background : '#0f171e' }}>
          <MovieListing/>
       </div>
-    )
+    )         
   }
 }
 
