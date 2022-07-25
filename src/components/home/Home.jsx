fda5e9646b9ae0384b5a5761b91f307d93fd3d82
chamberlince@icloud.com <chamberlince@icloud.com>
2022-07-25 13:17:26 -0400

    git API

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
+import {getMovieById, getAllMovies} from '../utils/movieList.js'
+
+const allMovies = getAllMovies();
+console.log(allMovies);
+const movieData = getMovieById('tt0314166');
+console.log(movieData);
 
 export class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <div>{movieData.Title}</div>
     )
   }
 }
